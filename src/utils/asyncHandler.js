46f85c3bcfc9e16f6982124898ddab6ad4ac5d54
Rahul chauhan <rahulchauhah50@gmail.com>
2026-05-16 22:31:43 +0530

    phase

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,7 +1,13 @@
 // Wrapper for async route handlers to catch errors automatically
 const asyncHandler = (fn) => {
   return (req, res, next) => {
-    Promise.resolve(fn(req, res, next)).catch(next);
+    return Promise.resolve(fn(req, res, next)).catch((err) => {
+      if (typeof next === 'function') {
+        return next(err);
+      }
+      // No next provided (called directly in tests) - rethrow so caller can catch
+      throw err;
+    });
   };
 };
 
